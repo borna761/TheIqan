1efac8d7310e258afb297d33b367c2a03bc8a971
Borna Safai <borna@paradem.co>
2016-01-28 17:04:24 -0500

    Showing themes and subtheme paragraphs

--- app/models/iqan.rb
+++ app/models/iqan.rb
@@ -1,4 +1,6 @@
 class Iqan < ActiveRecord::Base
 	has_many :themeparagraphs
+	has_many :themes, through: :themeparagraphs
 	has_many :subthemeparagraphs
+	has_many :subthemes, through: :subthemeparagraphs
 end
